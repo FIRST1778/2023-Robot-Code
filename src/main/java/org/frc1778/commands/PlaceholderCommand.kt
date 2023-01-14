65368d593df1f7cf7167f05e65c9026a242503e5
gamingnight5965 <34695615+gamingnight5965@users.noreply.github.com>
2023-01-14 09:30:41 -0800

    initial commit

---
+++ src/main/java/org/frc1778/commands/PlaceholderCommand.kt
@@ -0,0 +1,11 @@
+package org.frc1778.commands
+
+import edu.wpi.first.wpilibj2.command.CommandBase
+
+class PlaceholderCommand : CommandBase()
+{
+    init
+    {
+        // Call addRequirements() here to declare subsystem dependencies.
+    }
+}
