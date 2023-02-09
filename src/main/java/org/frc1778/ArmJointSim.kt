f5ca2991ec75660a92fe3d439f09ab999097398a
Apollo Graves <apollo.graves@gmail.com>
2023-02-08 20:00:38 -0800

    Added simulation for arm joint

---
+++ src/main/java/org/frc1778/ArmJointSim.kt
@@ -0,0 +1,76 @@
+import edu.wpi.first.math.system.plant.DCMotor
+import edu.wpi.first.wpilibj.Timer
+import java.io.File
+import java.io.PrintWriter
+
+class ArmJointSim(initialJontAngle: Double){
+    private var velocity = 0.0
+    private var accel = 0.0
+    private var joint_theta = initialJontAngle
+    private var Ng = 80
+    private var motor = DCMotor.getNEO(1)
+    private var motor_count = 2
+    private var arm_mass = 4.536 // kg
+    private var spring_spur_length = 0.0762 // m
+    private var spring_h_length = 0.381 // m
+    private var spring_constant = 845 // N / m
+    private var input = 0.0
+    private var currentDraw = 0.0
+    private var min_arm_length = 0.7 // m
+    private var max_arm_length = 2.0 // m
+    private var min_arm_inertia = 1.219 // m^2 kg
+    private var max_arm_inertia = 4.07 // m^2 kg
+    private var min_arm_cog = 0.3 // m
+    private var max_arm_cog = 0.6 // m
+    fun makeLogger(): PrintWriter {
+        var file = File("logs.csv")
+        var logger = PrintWriter(file)
+        logger.println("time, velocity at joint (rad/s), angle at joint (deg), voltage, current")
+        return logger
+    }
+    private var logger = makeLogger()
+    fun setInput(voltage: Double){
+        input = voltage
+    }
+    fun update(dt: Double, arm_length: Double){
+        calculateAcceleration(arm_length)
+        capVelocity(dt)
+        joint_theta += velocityAtJoint() * dt
+        val time: Double = Timer.getFPGATimestamp()
+        logger.printf("%s,%s,%s,%s,%s\n", time, velocityAtJoint(), Math.toDegrees(joint_theta), input, currentDraw)
+    }
+    fun capVelocity(dt : Double){
+        velocity += accel * dt
+        if (velocity > motor.freeSpeedRadPerSec){
+            velocity = motor.freeSpeedRadPerSec
+        }
+        if(velocity < -motor.freeSpeedRadPerSec){
+            velocity = -motor.freeSpeedRadPerSec
+        }
+    }
+    fun calculateAcceleration(arm_length: Double){
+        currentDraw = motor.getCurrent(velocity, input)
+        val torqueAtMotor: Double = motor.getTorque(currentDraw)
+        val inertia: Double = calculateInertia(arm_length)
+        val armTorqueGravity: Double = 9.81 * arm_mass * calculateCog(arm_length) * Math.sin(joint_theta)
+        val springTorque: Double = spring_constant * spring_spur_length * spring_h_length * Math.sin(joint_theta)
+        accel = Ng * (Ng * motor_count * torqueAtMotor + springTorque - armTorqueGravity) / inertia
+    }
+    fun velocityAtJoint(): Double {
+        return velocity / Ng
+    }
+    fun calculateInertia(arm_length: Double): Double {
+        return min_arm_inertia + (max_arm_inertia - min_arm_inertia) / (max_arm_length - min_arm_length) * (arm_length - min_arm_length)
+    }
+
+    fun calculateCog(arm_length: Double): Double {
+        return min_arm_cog + (max_arm_cog - min_arm_cog) / (max_arm_length - min_arm_length) * (arm_length - min_arm_length)
+    }
+    fun getCurrentDrawAmps(): Double {
+        return motor_count * currentDraw
+    }
+
+    fun getMinArmLength(): Double {
+        return min_arm_length
+    }
+}
--- src/main/java/org/frc1778/Robot.kt
+++ src/main/java/org/frc1778/Robot.kt
@@ -1,13 +1,16 @@
 package org.frc1778
 
+import ArmJointSim
 import com.ctre.phoenix.sensors.CANCoder
 import com.pathplanner.lib.PathPlanner
 import com.pathplanner.lib.PathPlannerTrajectory
 import edu.wpi.first.networktables.NetworkTableInstance
-import edu.wpi.first.wpilibj.PneumaticHub
-import edu.wpi.first.wpilibj.PneumaticsModuleType
-import edu.wpi.first.wpilibj.TimedRobot
+import edu.wpi.first.wpilibj.*
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
+import edu.wpi.first.wpilibj.simulation.BatterySim
+import edu.wpi.first.wpilibj.simulation.EncoderSim
+import edu.wpi.first.wpilibj.simulation.RoboRioSim
 import edu.wpi.first.wpilibj.smartdashboard.Field2d
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.Command
@@ -17,7 +20,7 @@ import org.frc1778.lib.SwerveTrajectoryTrackerCommand
 import org.frc1778.subsystems.Drive
 import org.ghrobotics.lib.wrappers.FalconDoubleSolenoid
 import org.ghrobotics.lib.wrappers.FalconSolenoid
-import org.ghrobotics.lib.wrappers.FalconTimedRobot
+import org.frc1778.lib.FalconTimedRobot
 import javax.naming.ldap.Control
 
 /**
@@ -31,15 +34,19 @@ import javax.naming.ldap.Control
  * object or package, it will get changed everywhere.)
  */
 object Robot : FalconTimedRobot() {
-
-
     private val field = Field2d()
     private val fieldTab = Shuffleboard.getTab("Field")
     val trajectory = PathPlanner.loadPath("Trajectory Test", 4.00, 1.00)
     lateinit var trajectoryCommand: SwerveTrajectoryTrackerCommand
     private var autonomousCommand: Command? = null
+    var armJointSim = ArmJointSim(0.0)
 
-//    val pcm = PneumaticHub(30)
+    var encoder = Encoder(1, 2, false, CounterBase.EncodingType.k4X)
+    var encoderSim = EncoderSim(encoder)
+
+    var motor = PWMSparkMax(2)
+
+    //    val pcm = PneumaticHub(30)
 //    val compressor = pcm.makeCompressor()
 //
 //    val sol = FalconDoubleSolenoid(
@@ -58,12 +65,17 @@ object Robot : FalconTimedRobot() {
         // button bindings, and put our autonomous chooser on the dashboard.
         RobotContainer
         SmartDashboard.setNetworkTableInstance(
-            NetworkTableInstance.getDefault()
+                NetworkTableInstance.getDefault()
         )
         Drive.pigeon.yaw = 0.0
         field.getObject("traj").setTrajectory(trajectory)
         fieldTab.add("Field", field).withSize(8, 4)
 
+        encoder.setSamplesToAverage(5)
+
+        encoder.setDistancePerPulse(1.0 / 360.0 * 2.0 * Math.PI * 1.5)
+
+        encoder.setMinRate(1.0)
     }
 
 
@@ -109,11 +121,21 @@ object Robot : FalconTimedRobot() {
 
     /** This method is called periodically during operator control.  */
     override fun teleopPeriodic() {
-//        println(compressor.pressure)
-//        Controls.operatorController.update()
-
+        motor.set(0.5)
     }
 
+    override fun simulationPeriodic() {
+        armJointSim.setInput(motor.get() * RobotController.getBatteryVoltage())
+
+        armJointSim.update(0.02, armJointSim.getMinArmLength())
+
+        encoderSim.setRate(armJointSim.velocityAtJoint())
+        RoboRioSim.setVInVoltage(BatterySim.calculateDefaultBatteryLoadedVoltage(armJointSim.getCurrentDrawAmps()))
+    }
+
+    override fun simulationInit() {
+
+    }
 }
 
 //    override fun testInit()
