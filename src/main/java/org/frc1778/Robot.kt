aacc759a09aa7ff15e2aa1dcd31eede152e245d9
gamingnight5965 <34695615+gamingnight5965@users.noreply.github.com>
2023-02-16 21:46:10 -0800

    Testing for Pathfinding

--- src/main/java/org/frc1778/Robot.kt
+++ src/main/java/org/frc1778/Robot.kt
@@ -1,9 +1,15 @@
 package org.frc1778
 
+import com.pathplanner.lib.PathConstraints
 import com.pathplanner.lib.PathPlanner
 import com.pathplanner.lib.PathPlannerTrajectory
+import com.pathplanner.lib.PathPoint
+import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.math.geometry.Translation2d
+import edu.wpi.first.math.trajectory.Trajectory
+import edu.wpi.first.math.trajectory.constraint.SwerveDriveKinematicsConstraint
 import edu.wpi.first.networktables.NetworkTableInstance
-import edu.wpi.first.wpilibj.PneumaticsModuleType
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
 import edu.wpi.first.wpilibj.smartdashboard.Field2d
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
@@ -13,8 +19,16 @@ import org.frc1778.lib.SwerveTrajectoryTrackerCommand
 import org.frc1778.subsystems.Drive
 import org.frc1778.subsystems.Lights
 import org.frc1778.subsystems.Vision
-import org.ghrobotics.lib.wrappers.FalconDoubleSolenoid
+import org.ghrobotics.lib.mathematics.twodim.geometry.Rectangle2d
+import org.ghrobotics.lib.mathematics.twodim.trajectory.FalconTrajectoryConfig
+import org.ghrobotics.lib.mathematics.twodim.trajectory.FalconTrajectoryGenerator
+import org.frc1778.lib.PathFinder
+import org.ghrobotics.lib.mathematics.units.SIUnit
+import org.ghrobotics.lib.mathematics.units.inches
+import org.ghrobotics.lib.mathematics.units.meters
 import org.ghrobotics.lib.wrappers.FalconTimedRobot
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 /**
  * The VM is configured to automatically run this object (which basically functions as a singleton class),
@@ -31,11 +45,12 @@ object Robot : FalconTimedRobot() {
 
     private val field = Field2d()
     private val fieldTab = Shuffleboard.getTab("Field")
-    private val trajectory: PathPlannerTrajectory = PathPlanner.loadPath("Trajectory Test", 4.00, 1.00)
+
+    //        private val trajectory: PathPlannerTrajectory = PathPlanner.loadPath("Trajectory Test", 4.00, 1.00)
     private lateinit var trajectoryCommand: SwerveTrajectoryTrackerCommand
     private var autonomousCommand: Command? = null
 
-//    val pcm = PneumaticHub(30)
+    //    val pcm = PneumaticHub(30)
 //    val compressor = pcm.makeCompressor()
 //
 //    val sol = FalconDoubleSolenoid(
@@ -59,8 +74,104 @@ object Robot : FalconTimedRobot() {
             NetworkTableInstance.getDefault()
         )
         Drive.pigeon.yaw = 0.0
+        //<editor-fold desc=" Path Gen Stuff">
+        val pf = PathFinder(
+            (36.25).inches,
+//            .25.meters,
+            //Red Substation
+            Rectangle2d(
+                Translation2d(0.00, 8.00), Translation2d(3.32, 5.50)
+            ),
+            Rectangle2d(
+                Translation2d(3.32, 8.00), Translation2d(6.70, 6.77)
+            ),
+            //Red Scoring Zone
+            Rectangle2d(
+                Translation2d(11.67, 0.00), Translation2d(15.15, 5.50)
+            ),
+            //Blue Charge Station
+            Rectangle2d(
+                Translation2d(4.86, 1.52), Translation2d(2.87, 3.96)
+            ),
+        )
+        val trajectoryConfig = FalconTrajectoryConfig(
+            SIUnit(7.00), SIUnit(7.00)
+        ).addConstraints(
+            SwerveDriveKinematicsConstraint(
+                Drive.kinematics, Constants.DriveConstants.maxSpeed.value
+            )
+        )
+        val points = pf.findPath(
+            Pose2d(
+                Translation2d(2.25, 2.75), Rotation2d(
+                    Math.toRadians(0.0)
+                )
+            ),
+            Pose2d(
+                Translation2d(15.00, 6.75), Rotation2d(
+                    Math.toRadians(0.0)
+                )
+//                Translation2d(9.00, 4.50), Rotation2d(0.0)
+            ),
+        )
+        val trajectory = points?.let {
+            infix fun Pose2d.headingTo(other: Pose2d): Rotation2d {
+                val relative = other.relativeTo(this)
+                return Rotation2d(relative.translation.x, relative.translation.y)
+            }
+
+            infix fun Pose2d.distanceTo(other: Pose2d): Double =
+                sqrt((this.x - other.x).pow(2) + (this.y - other.y).pow(2))
+
+
+            val pathPoints: MutableList<PathPoint> = mutableListOf()
+            pathPoints.add(
+                points.take(2).let {
+                        (firstPose, nextPose) ->
+                    PathPoint(
+                        firstPose.translation,
+                        firstPose headingTo nextPose,
+                        firstPose.rotation
+                    ).withPrevControlLength(
+                        sqrt(firstPose distanceTo nextPose)
+                    )
+                }
+            )
+            points.windowed(3).mapTo(pathPoints) { (prevPose, currPose, nextPose) ->
+                PathPoint(
+                    currPose.translation, prevPose headingTo currPose, currPose.rotation
+                ).withControlLengths(
+                    sqrt(prevPose distanceTo currPose),
+                    sqrt(currPose distanceTo  nextPose)
+                )
+            }
+            pathPoints.add(
+                points.takeLast(2).let {
+                    (prevPose, lastPose) ->
+                    PathPoint(
+                        lastPose.translation,
+                        prevPose headingTo lastPose,
+                        lastPose.rotation
+                    ).withPrevControlLength(
+                        sqrt(prevPose distanceTo lastPose)
+                    )
+                }
+            )
+
+
+            PathPlanner.generatePath(
+                PathConstraints(7.00, 7.00), pathPoints
+            )
+        } ?: PathPlannerTrajectory()
+//        val trajectory = points?.let {
+//            FalconTrajectoryGenerator.generateTrajectory(
+//                it, trajectoryConfig
+//            )
+//        } ?: Trajectory()
+        //</editor-fold>
         field.getObject("traj").setTrajectory(trajectory)
         fieldTab.add("Field", field).withSize(8, 4)
+        Drive.robotPosition = trajectory.initialPose
     }
 
 
@@ -82,8 +193,8 @@ object Robot : FalconTimedRobot() {
     }
 
     override fun autonomousInit() {
-        trajectoryCommand = Drive.followTrajectory(trajectory)
-        Drive.setPose(trajectory.initialHolonomicPose)
+//        trajectoryCommand = Drive.followTrajectory(trajectory)
+//        Drive.setPose(trajectory.initialHolonomicPose)
         autonomousCommand = trajectoryCommand
         autonomousCommand?.schedule()
 //        trajectoryCommand.schedule()
@@ -96,7 +207,7 @@ object Robot : FalconTimedRobot() {
 
     override fun teleopInit() {
         autonomousCommand?.cancel()
-        Drive.setPose(trajectory.initialHolonomicPose)
+//        Drive.setPose(trajectory.initialHolonomicPose)
 //        compressor.enableAnalog(
 //            30.0,
 //            40.0
@@ -110,6 +221,7 @@ object Robot : FalconTimedRobot() {
 //        Controls.operatorController.update()
 
     }
+
     override fun simulationInit() {
 
     }
---
+++ src/main/java/org/frc1778/subsystems/Intake.kt
@@ -0,0 +1,9 @@
+package org.frc1778.subsystems
+
+import org.ghrobotics.lib.commands.FalconSubsystem
+
+object Intake : FalconSubsystem() {
+
+
+
+}
