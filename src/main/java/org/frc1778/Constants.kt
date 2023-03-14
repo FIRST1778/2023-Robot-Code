0108cfca20eefd62ff55e5241d3e4bd6502122da
GraveyardHotDog <apollo.graves@gmail.com>
2023-03-13 18:30:34 -0700

    Robot No Deploy

--- src/main/java/org/frc1778/Constants.kt
+++ src/main/java/org/frc1778/Constants.kt
@@ -41,10 +41,10 @@ object Constants {
         private const val driveMotorEncoderNativeUnisPerRotation = 42.0 * driveReduction
 
         private val swerveDriveWheelOffsets = mapOf(
-            "Top Left" to 326.4,
-            "Top Right" to 18.4,
-            "Bottom Right" to 45.6,
-            "Bottom Left" to 273.0,
+            "Top Left" to 0.0,
+            "Top Right" to 0.0,
+            "Bottom Right" to 0.0,
+            "Bottom Left" to 0.0,
         )
         val topLeftSwerveModuleConstants = SwerveModuleConstants().apply {
             kName = "Top Left Swerve"
--- src/main/java/org/frc1778/commands/BalanceDriveCommand.kt
+++ src/main/java/org/frc1778/commands/BalanceDriveCommand.kt
@@ -1,7 +1,6 @@
 package org.frc1778.commands
 
 import edu.wpi.first.wpilibj.DriverStation
-import edu.wpi.first.wpilibj.DriverStation
 import edu.wpi.first.wpilibj.Timer
 import org.frc1778.Robot
 import org.frc1778.subsystems.Drive
@@ -9,61 +8,61 @@ import org.ghrobotics.lib.commands.FalconCommand
 import edu.wpi.first.math.geometry.Translation2d
 
 class BalanceDriveCommand : FalconCommand(Drive) {
-    companion object {
-        private const val MARGIN: Double = 0.2 // m
-        private const val VELOCITY: Double = 0.5 // m/sec
-        private const val FIELD_LENGTH: Double = 16.54 // m
-    }
-
-    private var aprilTagsWereEnabled = false
-    private var driveCommand
-
-    private fun adjustForAlliance(t: Translation2D) {
-        var x = t.x
-        if (DriverStation.alliance == DriverStation.Alliance.Red) {
-            x = FIELD_LENGTH - x
-        }
-        return Translation2D(x, t.y)
-    }
-
-    private fun makeCommand() {
-        val currChassisSpeeds = kinematics.toChassisSpeeds(*swerveModuleStates().toTypedArray())
-        val outOfCommunityPose = adjustForAlliance(Translation2D(5, 0))
-        val balanceBoardPose = adjustForAlliance(Translation2D(3, 0))
-        val trajectory = PathPlanner.generatePath(
-            PathConstraints(Drive.maxSpeed.value, 3.0),
-            PathPoint(
-                // field-relative position
-                outOfCommunityPose,
-                // heading (direction robot travels in)
-                Transform2d(Drive.robotPosition, outOfCommunityPose).translation.angle,
-                // holonomic rotation (direction robot is facing)
-                Drive.robotPosition.rotation
-            ),
-            PathPoint(
-                balanceBoardPose,
-                Transform2D(outOfCOmmunityPose, balanceBoardPose).translation.angle,
-                0.0
-            )
-        )
-        return Drive.followTrajectory(trajectory)
-    }
-
-    override fun initialize() {
-        aprilTagsWereEnabled = Drive.aprilTagsEnabled
-        Drive.aprilTagsEnabled = false
-        driveCommand = makeCommand()
-    }
-
-    override fun execute() {
-        
-    }
-
-    override fun isFinished(): Boolean {
-        return stage == xs.size
-    }
-
-    override fun end(interrupted: Boolean) {
-        Drive.aprilTagsEnabled = aprilTagsWereEnabled
-    }
+//    companion object {
+//        private const val MARGIN: Double = 0.2 // m
+//        private const val VELOCITY: Double = 0.5 // m/sec
+//        private const val FIELD_LENGTH: Double = 16.54 // m
+//    }
+//
+//    private var aprilTagsWereEnabled = false
+//    private var driveCommand
+//
+//    private fun adjustForAlliance(t: Translation2D) {
+//        var x = t.x
+//        if (DriverStation.alliance == DriverStation.Alliance.Red) {
+//            x = FIELD_LENGTH - x
+//        }
+//        return Translation2D(x, t.y)
+//    }
+//
+//    private fun makeCommand() {
+//        val currChassisSpeeds = kinematics.toChassisSpeeds(*swerveModuleStates().toTypedArray())
+//        val outOfCommunityPose = adjustForAlliance(Translation2D(5, 0))
+//        val balanceBoardPose = adjustForAlliance(Translation2D(3, 0))
+//        val trajectory = PathPlanner.generatePath(
+//            PathConstraints(Drive.maxSpeed.value, 3.0),
+//            PathPoint(
+//                // field-relative position
+//                outOfCommunityPose,
+//                // heading (direction robot travels in)
+//                Transform2d(Drive.robotPosition, outOfCommunityPose).translation.angle,
+//                // holonomic rotation (direction robot is facing)
+//                Drive.robotPosition.rotation
+//            ),
+//            PathPoint(
+//                balanceBoardPose,
+//                Transform2D(outOfCOmmunityPose, balanceBoardPose).translation.angle,
+//                0.0
+//            )
+//        )
+//        return Drive.followTrajectory(trajectory)
+//    }
+//
+//    override fun initialize() {
+//        aprilTagsWereEnabled = Drive.aprilTagsEnabled
+//        Drive.aprilTagsEnabled = false
+//        driveCommand = makeCommand()
+//    }
+//
+//    override fun execute() {
+//
+//    }
+//
+//    override fun isFinished(): Boolean {
+//        return stage == xs.size
+//    }
+//
+//    override fun end(interrupted: Boolean) {
+//        Drive.aprilTagsEnabled = aprilTagsWereEnabled
+//    }
 }
