FIRST1778/2023-Robot-Code

commit a2bf3649b88e2f31de9dbf614b5d0d4099a59c8e

HolonomicDriveController Tuning

src/main/java/org/frc1778/Robot.kt
@@ -43,7 +43,7 @@ object Robot : FalconTimedRobot() {
 
     lateinit var zeroExtensionCommand: ZeroExtensionCommand
 
-    private val trajectory: PathPlannerTrajectory = PathPlanner.loadPath("Trajectory Test", 4.00, 1.00)
+    private val trajectory: PathPlannerTrajectory = PathPlanner.loadPath("Tuning Path", 1.5, 1.5)
     private lateinit var trajectoryCommand: SwerveTrajectoryTrackerCommand
     private var autonomousCommand: Command? = null
 
@@ -52,8 +52,8 @@ object Robot : FalconTimedRobot() {
 
 
     val driveInversion = when(alliance) {
-        Alliance.Red-> -1
-        else -> 1
+        Alliance.Red-> 1
+        else -> -1
     }
 
 
@@ -112,8 +112,8 @@ object Robot : FalconTimedRobot() {
         RobotContainer
 
 
-        Drive.pigeon.yaw = 0.0
-//        field.getObject("traj").setTrajectory(trajectory)
+//        Drive.pigeon.yaw = 0.0
+        field.getObject("traj").setTrajectory(trajectory)
         fieldTab.add("Field", field).withSize(8, 4)
 
         Arm.extensionControlEnabled = false
@@ -140,19 +140,19 @@ object Robot : FalconTimedRobot() {
     override fun autonomousInit() {
         Arm.resetIsZeroed() // DO NOT REMOVE
 //        Drive.resetPosition(Pose2d(1.85, 2.80, Rotation2d.fromDegrees(0.0)), Drive.modules.positions.toTypedArray())
-        Intake.retract()
-        Manipulator.open()
+//        Intake.retract()
+//        Manipulator.open()
 //        RobotContainer.getAutonomousCommand().schedule()
 
-        IntakeSpitCommand().withTimeout(7.5).schedule()
+//        IntakeSpitCommand().withTimeout(7.5).schedule()
 
         //zeroExtensionCommand = ZeroExtensionCommand()
         //zeroExtensionCommand.schedule()
 
-        //trajectoryCommand = Drive.followTrajectory(trajectory)
-        //Drive.setPose(trajectory.initialHolonomicPose)
-        //autonomousCommand = trajectoryCommand
-//        trajectoryCommand.schedule()
+        trajectoryCommand = Drive.followTrajectory(trajectory)
+        Drive.setPose(trajectory.initialHolonomicPose)
+        autonomousCommand = trajectoryCommand
+        trajectoryCommand.schedule()
 
     }
 
@@ -164,43 +164,43 @@ object Robot : FalconTimedRobot() {
     override fun teleopInit() {
         Arm.setDesiredAngle(Arm.getCurrentAngle())
 //        Arm.resetIsZeroed()
-        Drive.resetPosition(
-            when (alliance) {
-                Alliance.Blue -> {
-                    when (DriverStation.getLocation()) {
-                        1 -> Pose2d(
-                            Translation2d(1.85, 4.40), Rotation2d.fromDegrees(180.0)
-                        )
-
-                        2 -> Pose2d(
-                            Translation2d(1.85, 2.7), Rotation2d.fromDegrees(180.0)
-                        )
-
-                        else -> Pose2d(
-                            Translation2d(1.85, 1.05), Rotation2d.fromDegrees(180.0)
-                        )
-
-
-                    }
-                }
-
-                else -> {
-                    when (DriverStation.getLocation()) {
-                        1 -> Pose2d(
-                        Translation2d(14.75, 4.40), Rotation2d.fromDegrees(0.0)
-                    )
-
-                        2 -> Pose2d(
-                        Translation2d(14.75, 2.7), Rotation2d.fromDegrees(0.0)
-                    )
-
-                        else -> Pose2d(
-                        Translation2d(14.75, 1.05), Rotation2d.fromDegrees(0.0)
-                    )
-                    }
-                }
-            }, Drive.modules.positions.toTypedArray()
-        )
+//        Drive.resetPosition(
+//            when (alliance) {
+//                Alliance.Blue -> {
+//                    when (DriverStation.getLocation()) {
+//                        1 -> Pose2d(
+//                            Translation2d(1.85, 4.40), Rotation2d.fromDegrees(180.0)
+//                        )
+//
+//                        2 -> Pose2d(
+//                            Translation2d(1.85, 2.7), Rotation2d.fromDegrees(180.0)
+//                        )
+//
+//                        else -> Pose2d(
+//                            Translation2d(1.85, 1.05), Rotation2d.fromDegrees(180.0)
+//                        )
+//
+//
+//                    }
+//                }
+//
+//                else -> {
+//                    when (DriverStation.getLocation()) {
+//                        1 -> Pose2d(
+//                        Translation2d(14.75, 4.40), Rotation2d.fromDegrees(0.0)
+//                    )
+//
+//                        2 -> Pose2d(
+//                        Translation2d(14.75, 2.7), Rotation2d.fromDegrees(0.0)
+//                    )
+//
+//                        else -> Pose2d(
+//                        Translation2d(14.75, 1.05), Rotation2d.fromDegrees(0.0)
+//                    )
+//                    }
+//                }
+//            }, Drive.modules.positions.toTypedArray()
+//        )
 //        ArmAngleCommand(110.0.degrees).schedule()
 //        ZeroExtensionCommand().schedule()
 //

src/main/deploy/pathplanner/Tuning Path.path
@@ -1,0 +1,74 @@
+{
+  "waypoints": [
+    {
+      "anchorPoint": {
+        "x": 8.6133202794223,
+        "y": 0.6518211039275126
+      },
+      "prevControl": null,
+      "nextControl": {
+        "x": 9.445165349820158,
+        "y": 0.6669810897890386
+      },
+      "holonomicAngle": 0,
+      "isReversal": false,
+      "velOverride": null,
+      "isLocked": false,
+      "isStopPoint": false,
+      "stopEvent": {
+        "names": [],
+        "executionBehavior": "parallel",
+        "waitBehavior": "none",
+        "waitTime": 0
+      }
+    },
+    {
+      "anchorPoint": {
+        "x": 9.6379238263471,
+        "y": 2.2090489020045743
+      },
+      "prevControl": {
+        "x": 9.58284997591083,
+        "y": 1.392120120533249
+      },
+      "nextControl": {
+        "x": 9.58284997591083,
+        "y": 1.392120120533249
+      },
+      "holonomicAngle": 180.0,
+      "isReversal": true,
+      "velOverride": null,
+      "isLocked": false,
+      "isStopPoint": false,
+      "stopEvent": {
+        "names": [],
+        "executionBehavior": "parallel",
+        "waitBehavior": "none",
+        "waitTime": 0
+      }
+    },
+    {
+      "anchorPoint": {
+        "x": 10.660267254913968,
+        "y": 0.6518211039275126
+      },
+      "prevControl": {
+        "x": 9.83068230287404,
+        "y": 0.5935492892073465
+      },
+      "nextControl": null,
+      "holonomicAngle": 90.0,
+      "isReversal": false,
+      "velOverride": null,
+      "isLocked": false,
+      "isStopPoint": false,
+      "stopEvent": {
+        "names": [],
+        "executionBehavior": "parallel",
+        "waitBehavior": "none",
+        "waitTime": 0
+      }
+    }
+  ],
+  "markers": []
+}

src/main/java/org/frc1778/Constants.kt
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab
 import org.frc1778.lib.SwerveModuleConstants
 import org.ghrobotics.lib.mathematics.units.*
+import org.ghrobotics.lib.mathematics.units.derived.Acceleration
 import org.ghrobotics.lib.mathematics.units.derived.Radian
 import org.ghrobotics.lib.mathematics.units.derived.Velocity
 import org.ghrobotics.lib.mathematics.units.derived.degrees
@@ -25,23 +26,25 @@ import kotlin.math.hypot
 object Constants {
 
     object DriveConstants {
+
         val driveTab: ShuffleboardTab = Shuffleboard.getTab("Drive")!!
         const val wheelBase: Double = 23.5
         const val trackWidth: Double = 23.5
         private const val driveReduction = (14.0 / 50.0) * (27.0 / 17.0) * (15.0 / 45.0)
         val maxSpeed: SIUnit<Frac<Meter, Second>> = SIUnit((5676 / 60.0) * driveReduction * 4.inches.value * PI)
         val maxAngularSpeed: SIUnit<Velocity<Radian>> =
-            SIUnit(maxSpeed.value / hypot(trackWidth / 2.0, wheelBase / 2.0))
+            SIUnit((maxSpeed.value / hypot(trackWidth / 2.0, wheelBase / 2.0)))
+        val maxAngularAcceleration: SIUnit<Acceleration<Radian>> = SIUnit(maxAngularSpeed.value * (3.0))
 
         const val pigeonCanID: Int = 21
         private const val azimuthMotorEncoderNativeUnitsPerRotation = 21.5
         private const val driveMotorEncoderNativeUnisPerRotation = 42.0 * driveReduction
 
         private val swerveDriveWheelOffsets = mapOf(
-            "Top Left" to 328.1,
-            "Top Right" to 18.3,
-            "Bottom Right" to 44.7,
-            "Bottom Left" to 277.7,
+            "Top Left" to 327.8,
+            "Top Right" to 17.8,
+            "Bottom Right" to 45.5,
+            "Bottom Left" to 272.5,
         )
         val topLeftSwerveModuleConstants = SwerveModuleConstants().apply {
             kName = "Top Left Swerve"
@@ -197,8 +200,7 @@ object Constants {
         val ANGLE_MOTOR_UNIT_MODEL: NativeUnitRotationModel = NativeUnitRotationModel(2048.nativeUnits) //TODO
         val ZEROED_EXTENSION_DISTANCE_READING: SIUnit<Meter> = 7.9.inches
         val AORTransform: Transform3d = Transform3d(
-            Translation3d((-10.5).inches.value, 0.0, 49.5.inches.value),
-            Rotation3d()
+            Translation3d((-10.5).inches.value, 0.0, 49.5.inches.value), Rotation3d()
         )
         val ARM_EXTENSION_OFFSET = 36.5.inches
         val AllianceXOffset = 18.inches.value
@@ -209,8 +211,7 @@ object Constants {
         val APRIL_TAG_FIELD_LAYOUT =
             AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile)!!
         val CAMERA_ROBOT_TRANSFORM = Transform3d(
-            Translation3d((-10).inches.value, 9.0.inches.value, 42.inches.value),
-            Rotation3d(0.0, 0.0, 0.0)
+            Translation3d((-10).inches.value, 9.0.inches.value, 42.inches.value), Rotation3d(0.0, 0.0, 0.0)
         )
     }
 
@@ -224,20 +225,17 @@ object Constants {
 enum class Level(val transform: Transform3d) {
     Top(
         Transform3d(
-            Translation3d(39.73.inches.value, 0.0, 46.0.inches.value),
-            Rotation3d()
+            Translation3d(39.73.inches.value, 0.0, 46.0.inches.value), Rotation3d()
         )
     ),
     Middle(
         Transform3d(
-            Translation3d((22.7.inches).value,0.0 , 34.0.inches.value),
-            Rotation3d()
+            Translation3d((22.7.inches).value, 0.0, 34.0.inches.value), Rotation3d()
         )
     ),
     Bottom(
         Transform3d(
-            Translation3d(),
-            Rotation3d()
+            Translation3d(), Rotation3d()
         )
     );
 
@@ -250,44 +248,47 @@ enum class GamePiece(val heightOffset: Double) {
 
 //18" away from edge
 enum class Station(private val redPose: Pose3d, private val bluePose: Pose3d) {
-    Left(Pose3d(
-        Translation3d(),
-        Rotation3d()
-    ), Pose3d(
-        Translation3d(),
-        Rotation3d()
-    )), Center(Pose3d(
-        Translation3d(),
-        Rotation3d()
-    ), Pose3d(
-        Translation3d(),
-        Rotation3d()
-    )), Right(Pose3d(
-        Translation3d(),
-        Rotation3d()
-    ), Pose3d(
-        Translation3d(),
-        Rotation3d()
-    ));
+    Left(
+        Pose3d(
+            Translation3d(), Rotation3d()
+        ), Pose3d(
+            Translation3d(), Rotation3d()
+        )
+    ),
+    Center(
+        Pose3d(
+            Translation3d(), Rotation3d()
+        ), Pose3d(
+            Translation3d(), Rotation3d()
+        )
+    ),
+    Right(
+        Pose3d(
+            Translation3d(), Rotation3d()
+        ), Pose3d(
+            Translation3d(), Rotation3d()
+        )
+    );
 
     fun ours(): Pose3d {
-        return if (Robot.alliance == DriverStation.Alliance.Red)
-            redPose
-        else
-            bluePose
+        return if (Robot.alliance == DriverStation.Alliance.Red) redPose
+        else bluePose
     }
 }
 
 enum class Side(val transform: Transform3d) {
-    Left(Transform3d(
-        Translation3d(
-            0.0, 22.0.inches.value, 0.0
-        ),
-        Rotation3d()
-    )), Right(Transform3d(
-        Translation3d(
-            0.0, -22.inches.value, 0.0
-        ),
-        Rotation3d()
-    ))
+    Left(
+        Transform3d(
+            Translation3d(
+                0.0, 22.0.inches.value, 0.0
+            ), Rotation3d()
+        )
+    ),
+    Right(
+        Transform3d(
+            Translation3d(
+                0.0, -22.inches.value, 0.0
+            ), Rotation3d()
+        )
+    )
 }

src/main/java/org/frc1778/commands/SwerveTrajectoryTrackerCommand.kt
@@ -8,6 +8,7 @@
 
 package org.frc1778.commands
 
+import com.pathplanner.lib.PathPlannerTrajectory
 import org.frc1778.lib.FalconSwerveDrivetrain
 import edu.wpi.first.math.kinematics.SwerveModuleState
 import edu.wpi.first.math.trajectory.Trajectory
@@ -22,14 +23,14 @@ import org.ghrobotics.lib.utils.Source
 
 class SwerveTrajectoryTrackerCommand(
     private val drivetrain: FalconSwerveDrivetrain<*>,
-    private val trajectorySource: Source<Trajectory>
+    private val trajectorySource: Source<PathPlannerTrajectory>
 ) : FalconCommand(drivetrain) {
 
     private var prevStates = Array(4) { SwerveModuleState() }
 
     private val timer = Timer()
     private var elapsed = 0.0
-    private lateinit var trajectory: Trajectory
+    private lateinit var trajectory: PathPlannerTrajectory
 
     override fun initialize() {
         trajectory = trajectorySource()
@@ -42,12 +43,12 @@ class SwerveTrajectoryTrackerCommand(
 
     override fun execute() {
         elapsed = timer.get()
-        val currentTrajectoryState = trajectory.sample(elapsed)
+        val currentTrajectoryState = trajectory.sample(elapsed) as PathPlannerTrajectory.PathPlannerState
 
         val chassisSpeeds = drivetrain.controller.calculate(
             drivetrain.robotPosition,
             currentTrajectoryState,
-            currentTrajectoryState.poseMeters.rotation
+            currentTrajectoryState.holonomicRotation
         )
 
         val wheelStates = drivetrain.kinematics.toSwerveModuleStates(chassisSpeeds)

src/main/java/org/frc1778/commands/SwerveTrajectoryTrackerWithMarkersCommand.kt
@@ -28,7 +28,7 @@ class SwerveTrajectoryTrackerWithMarkersCommand(
 
     private val timer = Timer()
     private var elapsed = 0.0
-    private lateinit var trajectory: Trajectory
+    private lateinit var trajectory: PathPlannerTrajectory
     private lateinit var trajectoryFollowingCommand: SwerveTrajectoryTrackerCommand
 
     init {

src/main/java/org/frc1778/lib/FalconSwerveDrivetrain.kt
@@ -208,7 +208,7 @@ abstract class FalconSwerveDrivetrain<T : AbstractFalconSwerveModule<*, *>> :
             this,
             forwardInput * outputLimiter,
             strafeInput * outputLimiter,
-            rotationInput * outputLimiter,
+            rotationInput * outputLimiter * .75,
             fieldRelative
         )
         val states = kinematics.toSwerveModuleStates(speeds)
@@ -222,13 +222,13 @@ abstract class FalconSwerveDrivetrain<T : AbstractFalconSwerveModule<*, *>> :
         poseEstimator.resetPosition(gyro(), positions, pose)
     }
 
-    fun followTrajectory(trajectory: Trajectory, mirrored: Boolean = false) =
-        SwerveTrajectoryTrackerCommand(this, Source(if (mirrored) trajectory.mirror() else trajectory))
+    fun followTrajectory(trajectory: PathPlannerTrajectory, mirrored: Boolean = false) =
+        SwerveTrajectoryTrackerCommand(this, Source((if (mirrored) trajectory.mirror() else trajectory) as PathPlannerTrajectory))
 
-    fun followTrajectory(trajectory: Trajectory, mirrored: BooleanSource) =
-        SwerveTrajectoryTrackerCommand(this, mirrored.map(trajectory.mirror(), trajectory))
+//    fun followTrajectory(trajectory: PathPlannerTrajectory, mirrored: BooleanSource) =
+//        SwerveTrajectoryTrackerCommand(this, {mirrored.map(trajectory.mirror(), trajectory) as PathPlannerTrajectory})
 
-    fun followTrajectory(trajectory: Source<Trajectory>) = SwerveTrajectoryTrackerCommand(this, trajectory)
+    fun followTrajectory(trajectory: Source<PathPlannerTrajectory>) = SwerveTrajectoryTrackerCommand(this, trajectory)
 
     fun followTrajectoryWithCommands(trajectory: Source<PathPlannerTrajectory>, eventMap: HashMap<String, Command>) =
         SwerveTrajectoryTrackerWithMarkersCommand(this, trajectory, eventMap)

src/main/java/org/frc1778/subsystems/Drive.kt
@@ -21,6 +21,7 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition
 import edu.wpi.first.math.trajectory.Trajectory
 import edu.wpi.first.math.trajectory.TrapezoidProfile
 import edu.wpi.first.networktables.GenericEntry
+import edu.wpi.first.util.sendable.SendableBuilder
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets
 import org.frc1778.Constants
 import org.frc1778.commands.PlaceGameObjectDriveCommand
@@ -54,9 +55,12 @@ object Drive : FalconSwerveDrivetrain<FalconNeoSwerveModule>() {
 
     val driveLogger = DataLogger("Drive")
     init {
-        driveLogger.add("yaw", {-> pigeon.yaw})
-        driveLogger.add("pitch", {-> pigeon.pitch})
-        driveLogger.add("roll", {-> pigeon.roll})
+        driveLogger.add("yaw") { -> pigeon.yaw }
+        driveLogger.add("pitch") { -> pigeon.pitch }
+        driveLogger.add("roll") { -> pigeon.roll }
+        driveLogger.add("X") { robotPosition.translation.x }
+        driveLogger.add("Y") { robotPosition.translation.y }
+        driveLogger.add("Rotation") {robotPosition.rotation.degrees}
     }
 
     public override val modules: List<FalconNeoSwerveModule> = listOf(
@@ -72,6 +76,7 @@ object Drive : FalconSwerveDrivetrain<FalconNeoSwerveModule>() {
         for (module in modules.reversed()) {
             Constants.DriveConstants.driveTab.add(module.name, module).withSize(3, 4)
         }
+        Constants.DriveConstants.driveTab.add("Drive", this).withSize(3,4)
         pigeon.configMountPose(Pigeon2.AxisDirection.PositiveX, Pigeon2.AxisDirection.PositiveZ, 500)
         modules.forEach {
             it.setAngle(0.0)
@@ -108,22 +113,22 @@ object Drive : FalconSwerveDrivetrain<FalconNeoSwerveModule>() {
     //TODO: Tune Holonomic Drive Controller
     override val controller: HolonomicDriveController = HolonomicDriveController(
         PIDController(
+            1.2,
             0.0,
-            0.0,
-            0.0
+            0.12
         ),
         PIDController(
+            1.2,
             0.0,
-            0.0,
-            0.0
+            0.12
         ),
         ProfiledPIDController(
+            0.2,
             0.0,
-            0.0,
-            0.0,
+            0.02,
             TrapezoidProfile.Constraints(
-                Constants.DriveConstants.maxSpeed.value,
-                Constants.DriveConstants.maxAngularSpeed.value
+                Constants.DriveConstants.maxAngularSpeed.value * 17.5,
+                Constants.DriveConstants.maxAngularAcceleration.value * 10.0
             )
 
         )
@@ -179,7 +184,7 @@ object Drive : FalconSwerveDrivetrain<FalconNeoSwerveModule>() {
 
 
     //TODO: Possibly use Pathfinding to generate a trajectory to our goal
-    fun trajectoryToGoal(): Trajectory? {
+    fun trajectoryToGoal(): PathPlannerTrajectory? {
         val currChassisSpeeds = kinematics.toChassisSpeeds(*swerveModuleStates().toTypedArray())
         if(scoringPose == null) return null
         return PathPlanner.generatePath(
@@ -217,6 +222,18 @@ object Drive : FalconSwerveDrivetrain<FalconNeoSwerveModule>() {
     }
 
 
+//    override fun periodic() {
+//        super.periodic() //DONT REMOVE
+//        driveLogger.log()
+//    }
+
+    override fun initSendable(builder: SendableBuilder?) {
+        super.initSendable(builder)
+        builder!!.addDoubleProperty("Max Angular Speed", {Constants.DriveConstants.maxAngularSpeed.value * 17.0
+        }, {})
+        builder.addDoubleProperty("Max Angular Accel", {Constants.DriveConstants.maxAngularAcceleration.value * 10.0
+        }, {})
+    }
 
 
 }

commit 328eeed4fb2d2d119c0b871ee3b8f8f67336aae8

Unswap red and blue alliances (bug introduced in a2bf3649)

src/main/java/org/frc1778/Robot.kt
@@ -57,8 +57,8 @@ object Robot : FalconTimedRobot() {
 
 
     val driveInversion = when(alliance) {
-        Alliance.Red-> 1
-        else -> -1
+        Alliance.Red-> -1
+        else -> 1
     }
 
 