f81465327830ea7152e404e8690a04e9edc36dc3
Simon Branch <simonmbranch@gmail.com>
2023-03-15 16:52:31 -0700

    BalanceCommand use pitch and roll

--- src/main/java/org/frc1778/commands/BalanceCommand.kt
+++ src/main/java/org/frc1778/commands/BalanceCommand.kt
@@ -25,34 +25,51 @@ class BalanceCommand: FalconCommand(Drive) {
         const val ERROR_DERIVATIVE_TOLERANCE: Double = 0.4
     }
 
-    var pitch = 0.0
-    var velocity = 0.0
+    var inclination: Double = 0.0
+    var velocity: Double = 0.0
 
     // Note that the PIDController defaults to being called every 20ms.  So we must
     // calculate a velocity ONCE per tick: not more, not less.
     lateinit var pid: PIDController
     override fun initialize() {
-        if (Math.abs(Drive.robotPosition.rotation.radians) > 0.1)
-            DriverStation.reportWarning("Smart balancing assumes 0 yaw", false)
         pid = PIDController(PROPORTIONAL, INTEGRAL, DERIVATIVE)
         pid.enableContinuousInput(0.0, 2.0*Math.PI)
         pid.setTolerance(ERROR_TOLERANCE, ERROR_DERIVATIVE_TOLERANCE)
-
     }
 
-
+    private fun boardInclination(): Double {
+        // Our robot is conceptually a rectangle, and a rectangle is basically a square,
+        // and squares are planes.  So think of robot rotation as defining a plane.
+        // Our goal is to find the angle between the robot plane and the ground, going in the
+        // X direction.
+        //
+        // A plane is defined uniquely by a point on it and a vector (starting at the point)
+        // perpendicular to the plane --- that is, pointing up.  The point will be (0,0,0)
+        // and the vector will be (0,0,1).  We rotate by yaw, pitch, and roll to find the
+        // actual current value of the vector.
+        //
+        // To find the angle of the plane along the X axis, we calculate a point on the plane
+        // (1, 0, z).  If the rotated vector ends at (a, b, c):
+        //     ax + by + cz = 0
+        // Substituting and rearranging:
+        //     a(1) + b(0) + cz = 0
+        //     a + cz = 0
+        //     cz = -a
+        //     z = -a/c
+        val rotation = Rotation3d(Drive.pigeon.yaw, Drive.pigeon.pitch, Drive.pigeon.roll)
+        val upVector = Translation3d(0.0, 0.0, 1.0).rotateBy(rotation)
+        val z: Double = -upVector.x/upVector.z
+        return atan(z)
+    }
 
     override fun execute() {
-        pitch = Math.toRadians(Drive.pigeon.pitch)
-        velocity = pid.calculate(sin(pitch), 0.0)
-        velocity = -velocity * Robot.driveInversion
-        if (cos(Drive.robotPosition.rotation.radians) < 0)
-            velocity = -velocity
+        inclination = boardInclination()
+        velocity = pid.calculate(sin(inclination), 0.0)
         Drive.swerveDrive(velocity, 0.0, 0.0, true)
     }
 
     override fun initSendable(builder: SendableBuilder?) {
-        builder?.addDoubleProperty("Pitch", {pitch},  {})
+        builder?.addDoubleProperty("Inclination", {inclination},  {})
         builder?.addDoubleProperty("Velocity", {velocity}, {})
     }
 
