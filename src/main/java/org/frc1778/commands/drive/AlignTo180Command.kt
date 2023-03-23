d84cbdc3171f8cbe908f654b976f02ceff5a427a
Simon Branch <simonmbranch@gmail.com>
2023-03-22 20:00:11 -0700

    Orient to nearest 180 degree increment, not towards grid
    Kyle's original desire for the command, because it lets him orient
    towards the driver station as well.  We can always revert it if it turns
    out to be unnecessary.

--- src/main/java/org/frc1778/commands/drive/AlignTo180Command.kt
+++ src/main/java/org/frc1778/commands/drive/AlignTo180Command.kt
@@ -1,5 +1,7 @@
 package org.frc1778.commands.drive
 
+import kotlin.math.PI
+import kotlin.math.round
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.math.kinematics.ChassisSpeeds
 import edu.wpi.first.math.trajectory.TrapezoidProfile
@@ -8,28 +10,22 @@ import org.frc1778.Robot
 import org.frc1778.subsystems.Drive
 import org.ghrobotics.lib.commands.FalconCommand
 
-class TurnToGridCommand : FalconCommand(Drive) {
-
+class AlignTo180Command : FalconCommand(Drive) {
     private lateinit var targetRotation: Rotation2d
 
     override fun initialize() {
-        targetRotation = Rotation2d.fromDegrees(
-            when (Robot.alliance) {
-                DriverStation.Alliance.Red -> 0.0
-                else -> 180.0
-            }
-        )
-
-
         val initialState = Drive.robotPosition.rotation
         val initialSpeeds = Drive.kinematics.toChassisSpeeds(*Drive.swerveModuleStates().toTypedArray())
+
+        // Round to 180
+        val goal = PI * round(initialState.radians / PI)
+        targetRotation = Rotation2d(goal)
 
         Drive.controller.thetaController.reset(
             TrapezoidProfile.State(
                 initialState.radians, initialSpeeds.omegaRadiansPerSecond
             )
         )
-
     }
 
     override fun execute() {
