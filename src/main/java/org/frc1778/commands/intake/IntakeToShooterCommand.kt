acfee4ec39e72dd6544d3faa02540f24afe00f97
gamingnight5965 <34695615+gamingnight5965@users.noreply.github.com>
2023-03-26 20:25:33 -0700

    Command fixes fixes

--- src/main/java/org/frc1778/commands/intake/IntakeToShooterCommand.kt
+++ src/main/java/org/frc1778/commands/intake/IntakeToShooterCommand.kt
@@ -1,29 +1,20 @@
 package org.frc1778.commands.intake
 
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.CommandBase
 import org.frc1778.Level
 import org.frc1778.subsystems.Intake
 import org.frc1778.subsystems.Shooter
 import org.ghrobotics.lib.commands.FalconCommand
 import org.ghrobotics.lib.commands.sequential
 
-class IntakeToShooterCommand: FalconCommand(Intake) {
+class IntakeToShooterCommand : FalconCommand(Intake) {
 
     override fun initialize() {
-        if(!Intake.cubeStored()) {
-            sequential {
-                +IntakeSuckCommand()
-                +this@IntakeToShooterCommand
-            }.schedule()
-        }
+        Intake.retract()
+        Intake.suck()
     }
 
-    override fun execute() {
-        Intake.retract()
-        if(Shooter.getScoringLevel() == Level.None) {
-            Intake.suck()
-            Shooter.suck()
-        }
-    }
 
     override fun end(interrupted: Boolean) {
         Intake.stop()
