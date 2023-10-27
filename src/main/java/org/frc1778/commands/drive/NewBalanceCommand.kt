45d235a28def9292c991022bdeafd01e5362c86e
Simon Branch <simonmbranch@gmail.com>
2023-10-26 17:21:46 -0700

    The new and old unite: PID and exponential backoff

--- src/main/java/org/frc1778/commands/drive/NewBalanceCommand.kt
+++ src/main/java/org/frc1778/commands/drive/NewBalanceCommand.kt
@@ -1,45 +1,44 @@
 package org.frc1778.commands.drive
 
+import edu.wpi.first.math.controller.PIDController
 import org.frc1778.commands.lights.BalanceAnimation
 import org.frc1778.subsystems.Drive
 import org.frc1778.subsystems.Gyro
 import org.ghrobotics.lib.commands.FalconCommand
 
 class BalanceCommand: FalconCommand(Drive) {
-    // Balance controller.  Velocity is proportional to sine of angle
-    // error.  Each time the balance board tips over, we decrease our
-    // velocity by a constant factor so that we stabilize in the center.
-    //
-    // The old, unreliable autonomous balance, which had no exponential
-    // backoff and used a PID controller, is in OldBalanceCommand.kt.
-    // To switch back to it, just rename the class back to
-    // BalanceCommand (the interface is the same).
-
-    private object Constants {
-        const val proportional: Double = 2.0  // (m/s) / m
-        const val derivative: Double = 0.125  // (m/s) / (m/s)
+    private object Config {
+        const val proportional: Double = 2.0 // (m/sec)/m
+        const val derivative: Double = 0.125
+        const val error_tolerance: Double = 0.055
+        const val derivative_tolerance: Double = 0.4
         const val backoff: Double = 0.4
-        const val tolerance: Double = 0.055  // m
-        const val errorTolerance: Double = 0.4
     }
 
     val balanceAnimation = BalanceAnimation()
 
-    var sign = Double.NaN
-    var multiplier = Double.NAN
+    var backoff = Double.NaN
+    var previousSign = Double.NaN
+    lateinit var pid: PIDController
 
     override fun initialize() {
-        var multiplier = 1.0
-        sign = 0.0
+        backoff = 1.0
+        previousSign = 0.0
         balanceAnimation.initialize()
+        pid = PIDController(Config.proportional, 0.0, Config.derivative)
+        pid.enableContinuousInput(0.0, 2.0*Math.PI)
+        pid.setTolerance(Config.error_tolerance, Config.derivative_tolerance)
     }
 
     override fun execute() {
         val inclination = Math.sin(Gyro.boardInclination())
-        if (Math.signum(inclination) * sign == -1.0)
-            factor *= Constants.backoff
-        sign = Math.signum(inclination)
-        Drive.swerveDrive(inclination * factor, 0.0, 0.0, true)
+        if (Math.signum(inclination) * previousSign == -1.0)
+            backoff *= Config.backoff
+        previousSign = Math.signum(inclination)
+
+        val xvel = backoff * pid.calculate(inclination, /* setpoint */ 0.0)
+        Drive.swerveDrive(xvel, 0.0, 0.0, true)
+
         balanceAnimation.execute()
     }
 
@@ -48,6 +47,6 @@ class BalanceCommand: FalconCommand(Drive) {
     }
 
     override fun isFinished(): Boolean {
-        return Math.abs(Math.sin(Gyro.boardInclination())) < Constants.tolerance
+        return pid.atSetpoint()
     }
 }
